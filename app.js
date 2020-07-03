37c8b0c5a9a4bcd4b87c20839dd73114cb5ed7ff
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-07-03 13:05:55 -0300

    Add Database connection

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 
+const connectDB = require('./config/db')
+
 dotenv.config({path: './config/config.env'})
+
+connectDB()
 
 const app = express()
 
