b0e624de3b942f3112f94224399b075ec085d107
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-07-03 15:26:13 -0300

    Add google passport strategy

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 
 const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'})
+
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -18,6 +22,15 @@ if (process.env.NODE_ENV === 'development') {
 
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', }))
 app.set('view engine', '.hbs')
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.static(path.join(__dirname, 'public')))
 
