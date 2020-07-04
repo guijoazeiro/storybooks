abde3e7545dc2bac66fe6691144c08401816f206
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-07-04 15:48:30 -0300

    Add logout

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,19 @@ const passport = require('passport')
 
 
 //GET /auth/google
-router.get('/google', passport.authenticate('google', {scope: ['profile'] }))
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 //GET //auth/google/callback
 router.get('/google/callback', passport.authenticate('google', {
     failureRedirect: '/',
 
-}), (req, res)=>{
+}), (req, res) => {
     res.redirect('/dashboard')
+})
+
+router.get('/logout', (req, res) => {
+req.logout()
+res.redirect('/')
 })
 
 module.exports = router
