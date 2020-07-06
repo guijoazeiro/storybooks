537defbaad5c7a57a69088b879f7330a1a3b7dd5
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-07-06 17:25:55 -0300

    Add public stories

--- app.js
+++ app.js
@@ -25,11 +25,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon} = require('./helpers/hbs')
 
 app.engine('.hbs', exphbs({
   helpers: {
-    formatDate
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon
   },
   defaultLayout: 'main',
   extname: '.hbs',
@@ -45,6 +48,11 @@ app.use(session({
 
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.use(function(req, res, next){
+  res.locals.user = req.user || null
+  next()
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
