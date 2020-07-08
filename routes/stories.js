7ec2142b4e43bb534b34c5b40fd2464846c7f71e
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-07-08 15:34:06 -0300

    Add delete story

--- routes/stories.js
+++ routes/stories.js
@@ -59,19 +59,44 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
 
 //PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id).lean()
+  try {
+    let story = await Story.findById(req.params.id).lean()
 
-  if (!story) {
-    return res.render('error/404')
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true
-    })
+    if (!story) {
+      return res.render('error/404')
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true
+      })
 
-    res.redirect('/dashboard')
+      res.redirect('/dashboard')
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
   }
 })
 
+// @route   DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      await Story.remove({ _id: req.params.id })
+      res.redirect('/dashboard')
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
 
 module.exports = router
