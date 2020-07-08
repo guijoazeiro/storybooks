7b8e09a6080d68a4a8b2b99dab6080bbbea29e4a
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-07-08 12:28:35 -0300

    Add edit story

--- app.js
+++ app.js
@@ -25,14 +25,15 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-const { formatDate, stripTags, truncate, editIcon} = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
 
 app.engine('.hbs', exphbs({
   helpers: {
     formatDate,
     stripTags,
     truncate,
-    editIcon
+    editIcon,
+    select
   },
   defaultLayout: 'main',
   extname: '.hbs',
